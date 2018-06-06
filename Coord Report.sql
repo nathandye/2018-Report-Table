d87c86d05bb3af88fe192335900ef6e51239f060
nathandye <nathandye@users.noreply.github.com>
2018-06-06 13:36:02 -0700

    Added LD to each metric
    Added LD

--- Coord Report.sql
+++ Coord Report.sql
@@ -5,7 +5,7 @@ INTO sp_wa_dyen.coord_report_data
 FROM (
 
 --Walk Attempts	
-	SELECT 	b.regionname, b.foname, 
+	SELECT 	b.regionname, b.foname, CAST (con.state_senate_district_latest as INT) as 'LD',
 			DATE (rep.reporting_week) as 'Reporting Week',
 			'Walk Attempts' as 'Metric',
 			CASE WHEN sta.id IS NOT NULL THEN 'x' ELSE NULL END as 'staff vs vol',
@@ -21,10 +21,10 @@ FROM (
 		AND DATE (con.datecanvassed) >= '2018-06-01'
 		AND b.committeeid = 59691
 		AND con.contacttype_name LIKE 'Walk'
-	GROUP BY 1,2,3,4,5
+	GROUP BY 1,2,3,4,5,6
 UNION
 --Walk Contacts	
-	SELECT 	b.regionname, b.foname, 
+	SELECT 	b.regionname, b.foname, CAST (con.state_senate_district_latest as INT) as 'LD',
 			DATE (rep.reporting_week) as 'Reporting Week',
 			'Walk Contacts' as 'Metric',
 			CASE WHEN sta.id IS NOT NULL THEN 'x' ELSE NULL END as 'staff vs vol',
@@ -41,10 +41,10 @@ UNION
 		AND b.committeeid = 59691
 		AND con.contacttype_name LIKE 'Walk'
 		AND con.successful_contact = 1
-	GROUP BY 1,2,3,4,5
+	GROUP BY 1,2,3,4,5,6
 UNION
 --DVC Phone Attempts	
-	SELECT 	b.regionname, b.foname, 
+	SELECT 	b.regionname, b.foname, CAST (con.state_senate_district_latest as INT) as 'LD',
 			DATE (rep.reporting_week) as 'Reporting Week',
 			'DVC Phone Attempts' as 'Metric',
 			CASE WHEN sta.id IS NOT NULL THEN 'x' ELSE NULL END as 'staff vs vol',
@@ -60,10 +60,10 @@ UNION
 		AND DATE (con.datecanvassed) >= '2018-06-01'
 		AND b.committeeid = 59691
 		AND con.contacttype_name LIKE 'Phone'
-	GROUP BY 1,2,3,4,5
+	GROUP BY 1,2,3,4,5,6
 UNION
 --DVC Phone Contacts	
-	SELECT 	b.regionname, b.foname, 
+	SELECT 	b.regionname, b.foname, CAST (con.state_senate_district_latest as INT) as 'LD',
 			DATE (rep.reporting_week) as 'Reporting Week',
 			'DVC Phone Contacts' as 'Metric',
 			CASE WHEN sta.id IS NOT NULL THEN 'x' ELSE NULL END as 'staff vs vol',
@@ -80,10 +80,10 @@ UNION
 		AND b.committeeid = 59691
 		AND con.contacttype_name LIKE 'Phone'
 		AND con.successful_contact = 1
-	GROUP BY 1,2,3,4,5
+	GROUP BY 1,2,3,4,5,6
 UNION
 --Active Volunteers		
-	SELECT 	b.regionname, b.foname, 
+	SELECT 	b.regionname, b.foname, ld.ld as 'LD', 
 			DATE (rep.reporting_week) as 'Reporting Week',
 			'Active Vols' as 'Metric',
 			NULL as 'staff vs vol',
@@ -99,11 +99,13 @@ UNION
 			on sub.myc_vanid = b.vanid
 		LEFT JOIN sp_wa_dyen.reporting_weeks as rep 
 			on rep.days = CURRENT_DATE 
+	       LEFT JOIN sp_wa_dyen.turf_to_ld as ld
+	               on ld.teamname = b.teamname
 		WHERE b.foname IS NOT NULL
-		GROUP BY 1,2,3
+		GROUP BY 1,2,3,4
 Union
 -- Lapsed Vols
-	SELECT 	b.regionname, b.foname, 
+	SELECT 	b.regionname, b.foname, ld.ld as 'LD', 
 			DATE (rep.reporting_week) as 'Reporting Week',
 			'Lapsed Vols' as 'Metric',
 			NULL as 'staff vs vol',
@@ -128,12 +130,14 @@ Union
 			on sub1.myc_vanid = b.vanid
 		LEFT JOIN sp_wa_dyen.reporting_weeks as rep 
 			on rep.days = CURRENT_DATE 
+	       LEFT JOIN sp_wa_dyen.turf_to_ld as ld
+	               on ld.teamname = b.teamname
 		WHERE sub2.myc_vanid IS NULL
 			AND b.foname IS NOT NULL
-		GROUP BY 1,2,3		
+		GROUP BY 1,2,3,4		
 UNION 
 --Vol Rec Calls	
-	SELECT 	b.regionname, b.foname, 
+	SELECT 	b.regionname, b.foname, ld.ld as 'LD', 
 			DATE (rep.reporting_week) as 'Reporting Week',
 			'Vol Rec Calls' as 'Metric',
 			CASE WHEN sta.id IS NOT NULL THEN 'x' ELSE NULL END as 'staff vs vol',
@@ -144,35 +148,39 @@ UNION
 		LEFT JOIN sp_wa_dyen.reporting_weeks as rep 
 			on rep.days = con.datecanvassed	
 		LEFT JOIN sp_wa_dyen.fo_user_ids as sta 
-			on sta.id = con.canvasserid	
+			on sta.id = con.canvasserid
+		LEFT JOIN sp_wa_dyen.turf_to_ld as ld
+	               on ld.teamname = b.teamname	
 	WHERE con.committeeid = 59691
 		AND DATE (con.datecanvassed) >= '2018-06-01'
 		AND con.call_attempt = 1
 		AND b.foname IS NOT NULL
-	GROUP BY 1,2,3,4,5
+	GROUP BY 1,2,3,4,5,6
 UNION
 --Vol Rec Contacts	
-	SELECT 	b.regionname, b.foname, 
+	SELECT 	b.regionname, b.foname, ld.ld as 'LD', 
 			DATE (rep.reporting_week) as 'Reporting Week',
 			'Vol Rec Contacts' as 'Metric',
 			CASE WHEN sta.id IS NOT NULL THEN 'x' ELSE NULL END as 'staff vs vol',
 			COUNT (*) as 'progress'
 	FROM org_sp_wa_vansync_live.contacts_myc_live as con 
 		LEFT JOIN org_sp_wa_vansync_live.dnc_activityregions as b 
-			on b.vanid = con.myc_vanid
+		      on b.vanid = con.myc_vanid
 		LEFT JOIN sp_wa_dyen.reporting_weeks as rep 
-			on rep.days = con.datecanvassed	
+		      on rep.days = con.datecanvassed	
 		LEFT JOIN sp_wa_dyen.fo_user_ids as sta 
-			on sta.id = con.canvasserid	
+		      on sta.id = con.canvasserid	
+		LEFT JOIN sp_wa_dyen.turf_to_ld as ld
+	              on ld.teamname = b.teamname
 	WHERE con.committeeid = 59691
 		AND DATE (con.datecanvassed) >= '2018-06-01'
 		AND con.successful_contact = 1
 		AND con.call_attempt = 1
 		AND b.foname IS NOT NULL
-	GROUP BY 1,2,3,4,5
+	GROUP BY 1,2,3,4,5,6
 UNION
 -- Host Conveyer Belt
-SELECT b.regionname, b.foname, 
+SELECT b.regionname, b.foname, ld.ld as 'LD', 
             DATE (rep.reporting_week)as 'Reporting Week',
             sub.active_type as 'Metric',
 			NULL as 'staff vs vol',
@@ -187,18 +195,20 @@ SELECT b.regionname, b.foname,
             WHERE eve.committeeid = 59691
                 AND eve.eventcalendarname IN ('Canvass', 'Phone Banks')
                 AND eve.mrr_statusname IN ('Completed', 'Walk In')
-               -- AND eve.eventdate > CURRENT_DATE - INTERVAL '30 Days'
+                AND eve.eventdate >= '2018-01-01'
             GROUP BY 1
         ) as sub
         LEFT JOIN org_sp_wa_vansync_live.dnc_activityregions as b     
             on sub.myc_vanid = b.vanid
         LEFT JOIN sp_wa_dyen.reporting_weeks as rep 
             on rep.days = CURRENT_DATE 
+        LEFT JOIN sp_wa_dyen.turf_to_ld as ld
+	              on ld.teamname = b.teamname
         WHERE b.foname IS NOT NULL
-        GROUP BY 1,2,3,4,5 
+        GROUP BY 1,2,3,4,5,6 
 UNION 
  --Vol Rec Calls Yesterday	
-SELECT b.regionname, b.foname, 
+SELECT b.regionname, b.foname, ld.ld as 'LD', 
 			DATE (rep.reporting_week) as 'Reporting Week',
 			'Vol Rec Calls Yesterday' as 'Metric',
 			NULL as 'staff vs vol',
@@ -208,14 +218,16 @@ SELECT b.regionname, b.foname,
 		 on b.vanid = con.myc_vanid
 	LEFT JOIN sp_wa_dyen.reporting_weeks as rep 
 			on rep.days = con.datecanvassed	
+	LEFT JOIN sp_wa_dyen.turf_to_ld as ld
+	              on ld.teamname = b.teamname
 	WHERE con.committeeid = 59691
 		AND con.call_attempt = 1
 		AND con.datecanvassed = CURRENT_DATE - INTERVAL '1 days'
 		AND b.foname IS NOT NULL
-	GROUP BY 1,2,3,4,5
+	GROUP BY 1,2,3,4,5,6
 UNION	
 -- Shifts Completed By Week
-SELECT c.regionname, c.foname, 
+SELECT c.regionname, c.foname, ld.ld as 'LD', 
 		DATE (rep.reporting_week) as 'Reporting Week', 
 		CASE WHEN a.eventcalendarname LIKE '1:1s' THEN '1:1s Completed'
 		     WHEN a.eventcalendarname LIKE 'Phone Banks' THEN 'DVC Phonebank Shifts Completed'
@@ -227,15 +239,17 @@ SELECT c.regionname, c.foname,
 			on rep.days = a.eventdate 
 		LEFT JOIN org_sp_wa_vansync_live.dnc_activityregions as c
 			on c.vanid = a.myc_vanid
+		LEFT JOIN sp_wa_dyen.turf_to_ld as ld
+	              on ld.teamname = c.teamname
 	WHERE a.attended = 1
 		AND a.committeeid = 59691
 		AND a.eventcalendarname IN  ('Canvass', 'Phone Banks','1:1s','Vol Recruitment')
 		AND DATE (a.eventdate) >= '2018-06-01'
 		AND c.foname IS NOT NULL
-	GROUP BY 1,2,3,4,5 
+	GROUP BY 1,2,3,4,5,6 
 UNION
 -- Shifts Completed Yesterday
-SELECT c.regionname, c.foname, 
+SELECT c.regionname, c.foname, ld.ld as 'LD', 
 		DATE (rep.reporting_week) as 'Reporting Week', 
 		CASE WHEN a.eventcalendarname LIKE '1:1s' THEN '1:1s Completed Yesterday'
 		     WHEN a.eventcalendarname LIKE 'Phone Banks' THEN 'DVC Phonebank Shifts Completed Yesterday'
@@ -247,11 +261,14 @@ SELECT c.regionname, c.foname,
 			on rep.days = a.eventdate 
 		LEFT JOIN org_sp_wa_vansync_live.dnc_activityregions as c
 			on c.vanid = a.myc_vanid
+		LEFT JOIN sp_wa_dyen.turf_to_ld as ld
+	              on ld.teamname = c.teamname
 	WHERE a.attended = 1
 		AND a.committeeid = 59691
 		AND a.eventcalendarname IN  ('Canvass', 'Phone Banks','1:1s','Vol Recruitment')
 		AND a.eventdate = CURRENT_DATE - INTERVAL '1 Days'
 		AND c.foname IS NOT NULL
-	GROUP BY 1,2,3,4,5
+	GROUP BY 1,2,3,4,5,6
 	
 ) as sub;
+
